07bd4341708dde5c55a66a2f3f9a909cce172e90
Arnaud Delorme <arnodelorme@gmail.com>
2020-02-25 17:03:25 -0600

    Allowing to import BIDS with multiple runs

--- pop_importbids.m
+++ pop_importbids.m
@@ -141,124 +141,140 @@ for iSubject = 2:size(bids.participants,1)
         allFiles = { eegFile.name };
         ind = strmatch( 'json', cellfun(@(x)x(end-3:end), allFiles, 'uniformoutput', false) );
         if ~isempty(ind)
-            eegFileJSON = allFiles{ind};
+            eegFileJSON = allFiles(ind);
             allFiles(ind) = [];
         end
         ind = strmatch( '.set', cellfun(@(x)x(end-3:end), allFiles, 'uniformoutput', false) );
         if ~isempty(ind)
-            eegFileRaw  = allFiles{ind};
+            eegFileRawAll  = allFiles(ind);
         elseif length(allFiles) == 1
-            eegFileRaw  = allFiles{1};
+            eegFileRawAll  = allFiles;
         else
             ind = strmatch( '.eeg', allFiles);
             if ~isempty(ind)
                 error(sprintf('No EEG data found for subject %s', bids.participants{iSubject,1}));
             end
-            eegFileRaw  = allFiles{ind};
-        end
-        [~,tmpFileName,fileExt] = fileparts(eegFileRaw);
-        eegFileRaw     = fullfile(subjectFolder{   iFold}, eegFileRaw);
-        eegFileNameOut = fullfile(subjectFolderOut{iFold}, [ tmpFileName '.set' ]);
-        
-        % extract task name
-        underScores = find(tmpFileName == '_');
-        if ~strcmpi(tmpFileName(underScores(end)+1:end), 'eeg')
-            error('EEG file name does not contain eeg'); % theoretically impossible
-        end
-        if isempty(findstr('ses', tmpFileName(underScores(end-1)+1:underScores(end)-1)))
-            task = tmpFileName(underScores(end-1)+1:underScores(end)-1);
+            eegFileRawAll  = allFiles(ind);
         end
         
         % skip most import if set file with no need for modication
-        if ~strcmpi(fileExt, '.set') || strcmpi(opt.bidsevent, 'on') || strcmpi(opt.bidschanloc, 'on') || ~strcmpi(opt.outputdir, bidsFolder)
-            switch lower(fileExt)
-                case '.set', % do nothing
-                    EEG = pop_loadset( eegFileRaw );
-                case {'.bdf','.edf'},
-                    EEG = pop_biosig( eegFileRaw );
-                case '.eeg',
-                    EEG = pop_loadbva( eegFileRaw );
-                otherwise
-                    error(sprintf('No EEG data found for subject %s', bids.participants{iSubject,1}));
+        for iFile = 1:length(eegFileRawAll)
+            
+            eegFileRaw = eegFileRawAll{iFile};
+            [~,tmpFileName,fileExt] = fileparts(eegFileRaw);
+            eegFileRaw     = fullfile(subjectFolder{   iFold}, eegFileRaw);
+            eegFileNameOut = fullfile(subjectFolderOut{iFold}, [ tmpFileName '.set' ]);
+            
+            % what is the run
+            iRun = 1;
+            ind = strfind(eegFileRaw, '_run-');
+            if ~isempty(ind)
+                iRun = str2double(eegFileRaw(ind(1)+5:ind(1)+6));
+                if isnan(iRun) || iRun == 0, error('Problem converting run information'); end
+            end
+                
+            % extract task name
+            underScores = find(tmpFileName == '_');
+            if ~strcmpi(tmpFileName(underScores(end)+1:end), 'eeg')
+                error('EEG file name does not contain eeg'); % theoretically impossible
+            end
+            if isempty(findstr('ses', tmpFileName(underScores(end-1)+1:underScores(end)-1)))
+                task = tmpFileName(underScores(end-1)+1:underScores(end)-1);
+            end
+
+            if ~strcmpi(fileExt, '.set') || strcmpi(opt.bidsevent, 'on') || strcmpi(opt.bidschanloc, 'on') || ~strcmpi(opt.outputdir, bidsFolder)
+                switch lower(fileExt)
+                    case '.set', % do nothing
+                        EEG = pop_loadset( eegFileRaw );
+                    case {'.bdf','.edf'},
+                        EEG = pop_biosig( eegFileRaw );
+                    case '.eeg',
+                        EEG = pop_loadbva( eegFileRaw );
+                    otherwise
+                        error(sprintf('No EEG data found for subject %s', bids.participants{iSubject,1}));
+                end
+
+                % channel location data
+                % ---------------------
+                if strcmpi(opt.bidschanloc, 'on')
+                    channelData = [];
+                    if ~isempty(channelFile)
+                        channelData = importtsv( fullfile(subjectFolder{iFold}, channelFile.name));
+                    end
+                    elecData = [];
+                    if ~isempty(elecFile)
+                        elecData = importtsv( fullfile(subjectFolder{iFold}, elecFile.name));
+                    end
+                    chanlocs = [];
+                    for iChan = 2:size(channelData,1)
+                        % the fields below are all required
+                        chanlocs(iChan-1).labels = channelData{iChan,1};
+                        chanlocs(iChan-1).type   = channelData{iChan,2};
+                        chanlocs(iChan-1).unit   = channelData{iChan,3};
+                        if size(channelData,2) > 3
+                            chanlocs(iChan-1).status = channelData{iChan,4};
+                        end
+                        if ~isempty(elecData)
+                            indElec = strmatch(chanlocs(iChan-1).labels, elecData(:,1), 'exact');
+                            chanlocs(iChan-1).X = elecData{indElec,2};
+                            chanlocs(iChan-1).Y = elecData{indElec,3};
+                            chanlocs(iChan-1).Z = elecData{indElec,4};
+                        end
+                    end
+                    if length(chanlocs) ~= EEG.nbchan
+                        error('Different number of channels in channel location file and EEG file');
+                    end
+                    if isfield(chanlocs, 'X')
+                        EEG.chanlocs = convertlocs(chanlocs, 'cart2all');
+                    else
+                        EEG.chanlocs = chanlocs;
+                    end
+                end
+
+                % event data
+                % ----------
+                if strcmpi(opt.bidsevent, 'on')
+                    eventData = [];
+                    if ~isempty(eventFile)
+                        eventData = importtsv( fullfile(subjectFolder{iFold}, eventFile.name));
+                    end
+                    events = struct([]);
+                    for iEvent = 2:size(eventData,1)
+                        events(end+1).latency  = eventData{iEvent,1}*EEG.srate+1; % convert to samples
+                        events(end).duration   = eventData{iEvent,2}*EEG.srate;   % convert to samples
+                        if size(eventData,2) > 2 && strcmpi(eventData{1,3}, 'trial_type')
+                            events(end).type = eventData{iEvent,3};
+                        end
+                        if size(eventData,2) > 3 && strcmpi(eventData{1,4}, 'response_time') && ~strcmpi(eventData{iEvent,4}, 'n/a')
+                            events(end+1).type   = 'response';
+                            events(end).latency  = (eventData{iEvent,1}+eventData{iEvent,4})*EEG.srate+1; % convert to samples
+                            events(end).duration = 0;
+                        end
+                    end
+                    EEG.event = events;
+                    EEG = eeg_checkset(EEG, 'eventconsistency');
+                end
+
+                % copy information inside dataset
+                EEG.subject = bids.participants{iSubject,1};
+                EEG.session = iFold;
+
+                if exist(subjectFolderOut{iFold}) ~= 7
+                    mkdir(subjectFolderOut{iFold});
+                end
+                EEG = pop_saveset( EEG, eegFileNameOut);
+            end
+
+            % building study command
+            commands = { commands{:} 'index' count 'load' eegFileNameOut 'subject' bids.participants{iSubject,1} 'session' iFold 'run' iRun };
+            
+            % custom fields
+            for iCol = 2:size(bids.participants,2)
+                commands = { commands{:} bids.participants{1,iCol} bids.participants{iSubject,iCol} };
             end
             
-            % channel location data
-            % ---------------------
-            if strcmpi(opt.bidschanloc, 'on')
-                channelData = [];
-                if ~isempty(channelFile)
-                    channelData = importtsv( fullfile(subjectFolder{iFold}, channelFile.name));
-                end
-                elecData = [];
-                if ~isempty(elecFile)
-                    elecData = importtsv( fullfile(subjectFolder{iFold}, elecFile.name));
-                end
-                chanlocs = [];
-                for iChan = 2:size(channelData,1)
-                    % the fields below are all required
-                    chanlocs(iChan-1).labels = channelData{iChan,1};
-                    chanlocs(iChan-1).type   = channelData{iChan,2};
-                    chanlocs(iChan-1).unit   = channelData{iChan,3};
-                    if size(channelData,2) > 3
-                        chanlocs(iChan-1).status = channelData{iChan,4};
-                    end
-                    if ~isempty(elecData)
-                        indElec = strmatch(chanlocs(iChan-1).labels, elecData(:,1), 'exact');
-                        chanlocs(iChan-1).X = elecData{indElec,2};
-                        chanlocs(iChan-1).Y = elecData{indElec,3};
-                        chanlocs(iChan-1).Z = elecData{indElec,4};
-                    end
-                end
-                if length(chanlocs) ~= EEG.nbchan
-                    error('Different number of channels in channel location file and EEG file');
-                end
-                if isfield(chanlocs, 'X')
-                    EEG.chanlocs = convertlocs(chanlocs, 'cart2all');
-                else
-                    EEG.chanlocs = chanlocs;
-                end
-            end
-            
-            % event data
-            % ----------
-            if strcmpi(opt.bidsevent, 'on')
-                eventData = [];
-                if ~isempty(eventFile)
-                    eventData = importtsv( fullfile(subjectFolder{iFold}, eventFile.name));
-                end
-                events = struct([]);
-                for iEvent = 2:size(eventData,1)
-                    events(end+1).latency  = eventData{iEvent,1}*EEG.srate+1; % convert to samples
-                    events(end).duration   = eventData{iEvent,2}*EEG.srate;   % convert to samples
-                    if size(eventData,2) > 2 && strcmpi(eventData{1,3}, 'trial_type')
-                        events(end).type = eventData{iEvent,3};
-                    end
-                    if size(eventData,2) > 3 && strcmpi(eventData{1,4}, 'response_time') && ~strcmpi(eventData{iEvent,4}, 'n/a')
-                        events(end+1).type   = 'response';
-                        events(end).latency  = (eventData{iEvent,1}+eventData{iEvent,4})*EEG.srate+1; % convert to samples
-                        events(end).duration = 0;
-                    end
-                end
-                EEG.event = events;
-                EEG = eeg_checkset(EEG, 'eventconsistency');
-            end
-            
-            % copy information inside dataset
-            EEG.subject = bids.participants{iSubject,1};
-            EEG.session = iFold;
-            
-            if exist(subjectFolderOut{iFold}) ~= 7
-                mkdir(subjectFolderOut{iFold});
-            end
-            EEG = pop_saveset( EEG, eegFileNameOut);
-        end
-        
-        % building study command
-        commands = { commands{:} 'index' count 'load' eegFileNameOut 'subject' bids.participants{iSubject,1} 'session' iFold };
-        for iCol = 2:size(bids.participants,2)
-            commands = { commands{:} bids.participants{1,iCol} bids.participants{iSubject,iCol} };
-        end
-        count = count+1;
+            count = count+1;
+        end % end for eegFileRaw
     end
 end
 
