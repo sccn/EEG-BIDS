5be17bf47a53a0a5922bf34efbb1767c877e4bab
Dung Truong <dt.young112@gmail.com>
2020-04-29 17:04:56 -0700

    Persistent data structure. Ok button handle. TODO reference

--- pop_eegacqinfo.m
+++ pop_eegacqinfo.m
@@ -40,117 +40,152 @@ function [ALLEEG,com] = pop_eegacqinfo(ALLEEG, varargin)
         listboxString = ['All' sets];
     end
     uicontrol('Style', 'text', 'string', 'Select set', 'Units', 'normalized','FontSize',13,'FontWeight','bold','BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.03 0.9 0.4 0.05]);
-    list = uicontrol(f, 'Style', 'listbox', 'String', listboxString, 'Units', 'normalized', 'Position', [0.03 0.1 0.3 0.8], 'Callback', @listSelectCB);
+    fileList = uicontrol(f, 'Style', 'listbox', 'String', listboxString, 'Units', 'normalized', 'Position', [0.03 0.1 0.3 0.8], 'Callback', @listSelectCB);
     uicontrol('Style', 'text', 'string', 'Add BIDS EEG acquisition information', 'Units', 'normalized','FontSize',13,'FontWeight','bold','BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.9 0.4 0.05]);
     uicontrol('Style', 'text', 'string', 'Cap manufacturer', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.83 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', '','tag', 'CapManufacturer', 'Units', 'normalized', 'Position', [0.78 0.83 0.2 0.05]);
+    uicontrol('Style', 'edit', 'string', '','tag', 'CapManufacturer', 'Units', 'normalized', 'Position', [0.78 0.83 0.2 0.05], 'Callback', @editedCB);
     uicontrol('Style', 'text', 'string', 'Cap model name', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.77 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', '','tag', 'CapManufacturersModelName', 'Units', 'normalized', 'Position', [0.78 0.77 0.2 0.05]);
+    uicontrol('Style', 'edit', 'string', '','tag', 'CapManufacturersModelName', 'Units', 'normalized', 'Position', [0.78 0.77 0.2 0.05], 'Callback', @editedCB);
     uicontrol('Style', 'text', 'string', 'EEG reference', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.71 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', '','tag', 'EEGreference', 'Units', 'normalized', 'Position', [0.78 0.71 0.2 0.05]);
+    uicontrol('Style', 'edit', 'string', '','tag', 'EEGreference', 'Units', 'normalized', 'Position', [0.78 0.71 0.2 0.05], 'Callback', @editedCB);
     uicontrol('Style', 'text', 'string', 'EEG ground', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.65 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', '','tag', 'EEGGround', 'Units', 'normalized', 'Position', [0.78 0.65 0.2 0.05]);
+    uicontrol('Style', 'edit', 'string', '','tag', 'EEGGround', 'Units', 'normalized', 'Position', [0.78 0.65 0.2 0.05], 'Callback', @editedCB);
     uicontrol('Style', 'text', 'string', 'EEG placement scheme (i.e. 10-20)', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.59 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', '','tag', 'EEGEEGPlacementScheme', 'Units', 'normalized', 'Position', [0.78 0.59 0.2 0.05]);
+    uicontrol('Style', 'edit', 'string', '','tag', 'EEGEEGPlacementScheme', 'Units', 'normalized', 'Position', [0.78 0.59 0.2 0.05], 'Callback', @editedCB);
     uicontrol('Style', 'text', 'string', 'Amplifier manufacturer', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.53 0.4 0.05]);
     uicontrol('Style', 'edit', 'string', '','tag', 'Manufacturer', 'Units', 'normalized', 'Position', [0.78 0.53 0.2 0.05]);    
-    uicontrol('Style', 'text', 'string', 'Amplifier model', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.47 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', '','tag', 'ManufacturersModelName', 'Units', 'normalized', 'Position', [0.78 0.47 0.2 0.05]); 
+    uicontrol('Style', 'text', 'string', 'Amplifier model', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.47 0.4 0.05], 'Callback', @editedCB);
+    uicontrol('Style', 'edit', 'string', '','tag', 'ManufacturersModelName', 'Units', 'normalized', 'Position', [0.78 0.47 0.2 0.05], 'Callback', @editedCB); 
     uicontrol('Style', 'text', 'string', 'Amplifier serial number', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.41 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', '','tag', 'DeviceSerialNumber', 'Units', 'normalized', 'Position', [0.78 0.41 0.2 0.05]);    
+    uicontrol('Style', 'edit', 'string', '','tag', 'DeviceSerialNumber', 'Units', 'normalized', 'Position', [0.78 0.41 0.2 0.05], 'Callback', @editedCB);    
     uicontrol('Style', 'text', 'string', 'Acquisition software version', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.35 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', '','tag', 'SoftwareVersions', 'Units', 'normalized', 'Position', [0.78 0.35 0.2 0.05]);    
+    uicontrol('Style', 'edit', 'string', '','tag', 'SoftwareVersions', 'Units', 'normalized', 'Position', [0.78 0.35 0.2 0.05], 'Callback', @editedCB);    
     uicontrol('Style', 'text', 'string', 'Hardware filters', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.29 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', 'n/a','tag', 'HardwareFilters', 'Units', 'normalized', 'Position', [0.78 0.29 0.2 0.05]);   
+    uicontrol('Style', 'edit', 'string', '','tag', 'HardwareFilters', 'Units', 'normalized', 'Position', [0.78 0.29 0.2 0.05], 'Callback', @editedCB);   
     uicontrol('Style', 'text', 'string', 'Software filters (freeform explanation)', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.23 0.4 0.05]);
-    uicontrol('Style', 'edit', 'string', 'n/a','tag', 'SoftwareFilters', 'Units', 'normalized', 'Position', [0.78 0.23 0.2 0.05]); 
+    uicontrol('Style', 'edit', 'string', '','tag', 'SoftwareFilters', 'Units', 'normalized', 'Position', [0.78 0.23 0.2 0.05], 'Callback', @editedCB); 
     uicontrol('Style', 'text', 'string', 'Country power line frequency', 'Units', 'normalized','FontSize',13,'BackgroundColor',bg,'ForegroundColor',fg,'HorizontalAlignment','left', 'Position', [0.38 0.17 0.4 0.05]);
-    uicontrol('Style', 'popupmenu', 'string', '50|60','tag', 'PowerLineFrequency', 'Units', 'normalized', 'Position', [0.78 0.17 0.2 0.05]); 
+    uicontrol('Style', 'popupmenu', 'string', {'Select','50','60'},'tag', 'PowerLineFrequency', 'Units', 'normalized', 'Position', [0.78 0.17 0.2 0.05], 'Callback', @editedCB); 
     
     % buttons
     uicontrol(f, 'Style', 'pushbutton', 'String', 'Help', 'FontSize',13, 'Units', 'normalized', 'Position', [0.38 0.02 0.12 0.05], 'Callback', @helpCB); 
     uicontrol(f, 'Style', 'pushbutton', 'String', 'Ok', 'FontSize',13, 'Units', 'normalized', 'Position', [0.85 0.02 0.12 0.05], 'Callback', @okCB); 
     uicontrol(f, 'Style', 'pushbutton', 'String', 'Cancel', 'FontSize',13, 'Units', 'normalized', 'Position', [0.7 0.02 0.12 0.05], 'Callback', @cancelCB); 
-%         { 'Style', 'edit', 'string', '' 'tag' 'CapManufacturer' }, ...
+    
+    eegBIDS = newEEGBIDS();
+
+    % wait
+    waitfor(f);
+    
     function listSelectCB(src,event) 
+       selected = src.String{src.Value};
+       % update GUI data
+       objs = findall(f);
+       for o=1:numel(objs)
+            if ~isempty(objs(o).Tag)
+                if strcmp(objs(o).Style, 'popupmenu')
+                    % if there's saved filter value, set popupmenu to
+                    % select it
+                    filter = eegBIDS(strcmp({eegBIDS.filename}, selected)).tInfo.(objs(o).Tag);
+                    if isempty(filter) || strcmp(filter, 'Select')
+                        objs(o).Value = find(strcmp(objs(o).String,'Select'));
+                    else
+                        objs(o).Value = find(strcmp(objs(o).String,filter));
+                    end
+                else
+                    objs(o).String = eegBIDS(strcmp({eegBIDS.filename}, selected)).tInfo.(objs(o).Tag);
+                end
+            end
+       end
        h = findobj('tag', 'EEGreference');
        h = h(1);
-       selected = src.String{src.Value};
        if ~strcmp(selected, 'All')
            h.String = ALLEEG(strcmp(sets,selected)).ref;
+           tmpsrc = [];
+           tmpsrc.Style = 'edit';
+           tmpsrc.String = h.String;
+           tmpsrc.Tag = 'EEGreference';
+           editedCB(tmpsrc,'');
        end
-       uicontrol(f, 'Style', 'text', 'String', src.String{src.Value}, 'Units', 'normalized', 'Position',[0.02 0 0.34 0.08], 'HorizontalAlignment', 'left', 'FontSize',11,'FontAngle','italic','ForegroundColor', fg,'BackgroundColor', bg);
+       uicontrol(f, 'Style', 'text', 'String', selected, 'Units', 'normalized', 'Position',[0.02 0 0.34 0.08], 'HorizontalAlignment', 'left', 'FontSize',11,'FontAngle','italic','ForegroundColor', fg,'BackgroundColor', bg);
     end
     function helpCB(src,event)
         pophelp('pop_eegacqinfo()');
     end
     function okCB(src,event)
-%         bids = [];
-%         tInfo = [];
-%         % preserve current BIDS info if have
-%         if isfield(STUDY, 'BIDS')
-%             bids = STUDY.BIDS;
-%             if isfield(bids, 'tInfo')
-%                 tInfo = bids.tInfo;
-%             end
-%         end
-%         % update tInfo from input fields
-%         objs = findall(f);
-%         for i=1:numel(objs)
-%             if ~isempty(objs(i).Tag)
-%                 if ~isempty(objs(i).String)
-%                     tInfo.(objs(i).Tag) = objs(i).String;
-%                 end
-%             end
-%         end
-%         % update STUDY BIDS structure
-%         bids.tInfo = tInfo;
-%         STUDY.BIDS = bids;
+        filenames = {eegBIDS.filename};
+        for i=1:numel(ALLEEG)
+            if ~isfield(ALLEEG(i), 'BIDS')
+                ALLEEG(i).BIDS = [];
+            end
+            
+            % finalize tInfo information for this set
+            objs = findall(f);
+            for o=1:numel(objs)
+                 tag = objs(o).Tag;
+                 if ~isempty(tag)
+                     % if individual set doesn't contain value for the tag
+                     % then use value of 'All'
+                     if isempty(eegBIDS(strcmp(filenames, ALLEEG(i).filename)).tInfo.(tag))
+                         eegBIDS(strcmp(filenames, ALLEEG(i).filename)).tInfo.(tag) = eegBIDS(strcmp(filenames,'All')).tInfo.(tag);
+                     end   
+                 end
+            end
+            
+            ALLEEG(i).BIDS.tInfo = eegBIDS(strcmp(filenames, ALLEEG(i).filename)).tInfo;
+        end
+        
         close(f);
     end
     function cancelCB(src, event)
         close(f);
     end
-% if nargin < 2
-%     uilist = { ...
-%         { 'Style', 'text', 'string', 'BIDS EEG acquisition information', 'fontweight', 'bold'  }, ...
-%         {} ...
-%         {} {} ...
-%         { 'Style', 'text', 'string', 'Cap manufacturer:' }, ...
-%         { 'Style', 'edit', 'string', '' 'tag' 'CapManufacturer' }, ...
-%         { 'Style', 'text', 'string', 'Cap model name:' }, ...
-%         { 'Style', 'edit', 'string', '' 'tag' 'CapManufacturersModelName' }, ...
-%         { 'Style', 'text', 'string', 'EEG reference:' }, ...
-%         { 'Style', 'edit', 'string', EEG(1).ref 'tag' 'EEGreference' }, ...
-%         { 'Style', 'text', 'string', 'EEG ground:' }, ...
-%         { 'Style', 'edit', 'string', '' 'tag' 'EEGGround' }, ...
-%         { 'Style', 'text', 'string', 'EEG placement scheme (i.e. 10-20):' }, ...
-%         { 'Style', 'edit', 'string', '' 'tag' 'EEGPlacementScheme' }, ...
-%         { 'Style', 'text', 'string', 'Amplifier manufacturer:' }, ...
-%         { 'Style', 'edit', 'string', '' 'tag' 'Manufacturer' }, ...
-%         { 'Style', 'text', 'string', 'Amplifier model:' }, ...
-%         { 'Style', 'edit', 'string', '' 'tag' 'ManufacturersModelName' }, ...
-%         { 'Style', 'text', 'string', 'Amplifier serial number:' }, ...
-%         { 'Style', 'edit', 'string', '' 'tag' 'DeviceSerialNumber' }, ...
-%         { 'Style', 'text', 'string', 'Acquisition software version:' }, ...
-%         { 'Style', 'edit', 'string', '' 'tag' 'SoftwareVersions' }, ...
-%         { 'Style', 'text', 'string', 'Hardware filters:' }, ...
-%         { 'Style', 'edit', 'string', 'n/a' 'tag' 'SoftwareFilters' }, ...
-%         { 'Style', 'text', 'string', 'Software filters (freeform explanation):' }, ...
-%         { 'Style', 'edit', 'string', 'n/a' 'tag' 'SoftwareFilters' }, ...
-%         { 'Style', 'text', 'string', 'Country power line frequency:' }, ...
-%         { 'Style', 'popupmenu', 'string', '50|60' 'tag' 'PowerLineFrequency' }, ...
-%         };
-%     geometry = cell(1, length(uilist)/2);
-%     geometry(:) = { [1 0.6] };
-%     geomvert = ones(1, length(geometry));
-%     geomvert(2) = 0.2;
-%     [results,userdata,~,restag] = inputgui( 'geometry', geometry, 'geomvert', geomvert, 'uilist', uilist, 'helpcom', 'pophelp(''pop_eegacqinfo'');', 'title', 'BIDS EEG data acquisition information -- pop_eegacqinfo()');
-%     if length(results) == 0, return; end
-% 
-% else
-%     options = varargin;
-% end
+    
+    function editedCB(src,event)
+        selected = fileList.String{fileList.Value};
+        bidsFileNames = {eegBIDS.filename};
+        if strcmp(src.Style, 'popupmenu')
+            input = src.String{src.Value};
+        else
+            input = src.String;
+        end
+        eegBIDS(strcmp(bidsFileNames, selected)).tInfo.(src.Tag) = input;
+    end
+
+    function bidsArray = newEEGBIDS()
+        objs = findall(f);
+        bidsArray = [];
+        allEEG = [];
+        allEEG.filename = 'All'; % same name appeared in listbox
+        tInfo = [];
+        for o=1:numel(objs)
+            if ~isempty(objs(o).Tag)
+                tInfo.(objs(o).Tag) = '';
+            end
+        end
+        allEEG.tInfo = tInfo;
+        bidsArray = [bidsArray allEEG];
+        
+        for i=1:numel(ALLEEG)
+            bids = [];
+            bids.filename = ALLEEG(i).filename;
+            tInfo = [];
+            if isfield(ALLEEG(i), 'BIDS')
+                bids = ALLEEG(i).BIDS;
+                if isfield(bids, 'tInfo')
+                    tInfo = bids.tInfo;
+                end
+            end
+
+            for o=1:numel(objs)
+                if ~isempty(objs(o).Tag)
+                    tInfo.(objs(o).Tag) = '';
+                end
+            end
+            bids.tInfo = tInfo;
+            bidsArray = [bidsArray bids];
+        end
+    end
 
 % history
 % -------
