b3069111d4e0fde88bc5671bfb251a590467158b
Dung Truong <dt.young112@gmail.com>
2020-07-06 10:32:17 -0700

    add documentation to pop_checkdatasetinfo and change prompt

--- pop_checkdatasetinfo.m
+++ pop_checkdatasetinfo.m
@@ -1,3 +1,22 @@
+% pop_checkdatasetinfo() - Check for consistency between STUDY and ALLEEG
+%                          Fields covered: filename, subject, condition,
+%                          group, session, run.
+%
+% Usage:
+%   >> [STUDY, ALLEEG] = pop_checkdatasetinfo(STUDY, ALLEEG);
+%                                              
+% Inputs:
+%   STUDY      - STUDY structure
+%
+%   ALLEEG     - array of all EEG dataset in STUDY
+% Outputs:
+%   STUDY      - If there's inconsistency and user select to copy info,
+%                returned STUDY contains the missing info copied from
+%                ALLEEG
+%
+%   ALLEEG     - Struct array of all EEG dataset in STUDY
+%
+% Author: Dung Truong, Arnaud Delorme
 function [STUDY, ALLEEG] = pop_checkdatasetinfo(STUDY, ALLEEG)
     datasetinfo = STUDY.datasetinfo;
     different = 0;
@@ -21,7 +40,7 @@ function [STUDY, ALLEEG] = pop_checkdatasetinfo(STUDY, ALLEEG)
     function yesCB(src, event)
         [STUDY, ALLEEG] = std_editset(STUDY, ALLEEG, 'updatedat', 'on');
         supergui( 'geomhoriz', { 1 1 1 }, 'uilist', { ...
-        { 'style', 'text', 'string', 'Information updated. Resave dataset(s) if you want to save the information on disk', 'HorizontalAlignment', 'center'}, { }, ...
+        { 'style', 'text', 'string', 'Information updated', 'HorizontalAlignment', 'center'}, { }, ...
         { 'style', 'pushbutton', 'string', 'Ok', 'callback', 'close(gcf);'}});
         waitfor(gcf);
         close(gcf); 
