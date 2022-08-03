4b2fa63ba1284e658d6cf28437d829825b829b73
Arnaud Delorme <arnodelorme@gmail.com>
2022-08-02 20:53:47 -0700

    fix issue with duration field duplicated

--- eeg_writeeventsfiles.m
+++ eeg_writeeventsfiles.m
@@ -104,7 +104,7 @@ if ~isempty(EEG.event)
                 opt.eInfo(end+1,:) = { 'onset' 'latency' };
             end
         end
-        if ~any(strcmp(bids_fields,'duration')) opt.eInfo(end+1,:) = { 'duration' 'duration' }; end
+        if ~any(strcmp(bids_fields,'duration')) && isfield(EEG.event, 'duration'), opt.eInfo(end+1,:) = { 'duration' 'duration' }; end
         if ~any(strcmp(bids_fields,'sample')) && isfield(EEG.event, 'latency'), opt.eInfo(end+1,:) = { 'sample' 'latency' }; end
         if ~any(strcmp(bids_fields,'value'))
             if isfield(EEG.event, 'value')
@@ -113,7 +113,6 @@ if ~isempty(EEG.event)
                 opt.eInfo(end+1,:) = { 'value' 'type' };
             end
         end
-        if ~any(strcmp(bids_fields,'duration')) && isfield(EEG.event, 'duration'), opt.eInfo(end+1,:) = { 'duration' 'duration' }; end
         if ~isempty(opt.trialtype), opt.eInfo(end+1,:) = { 'trial_type' 'xxxx' }; end
     end
     if ~isempty(opt.stimuli)
