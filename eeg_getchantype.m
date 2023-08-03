dedf19bdbf93a8f7095f6c230e6bfe852ea5096d
Arnaud Delorme <arnodelorme@gmail.com>
2023-08-02 14:24:08 -1000

    speed up by 10

--- eeg_getchantype.m
+++ eeg_getchantype.m
@@ -1,27 +1,24 @@
-function EEG = eeg_chantype(EEG)
-    % Assign channel types based on channel name
-    % types retrived from https://bids-specification.readthedocs.io/en/stable/glossary.html#objects.columns.type__eeg_channels
-    types = {'EEG', 'EMG', 'EOG', 'ECG', 'EKG', 'TRIG', 'GSR', 'PPG', 'MISC'};
-    for i = 1:length(EEG.chanlocs)
-        label = EEG.chanlocs(i).labels;
-        matchIdx = cellfun(@(x) contains(lower(label), lower(x)), types);
+function EEG = eeg_getchantype(EEG)
 
-        if any(matchIdx)
-            EEG.chanlocs(i).type = types{matchIdx};
-        elseif is_eeg_chan(label)
-            EEG.chanlocs(i).type = 'EEG';
-        end
-    end
+template_file = fullfile(fileparts(which('eeglab')),'sample_locs/Standard-10-20-Cap81.locs');
+if exist(template_file, 'file')
+    locs = readtable(template_file,'Delimiter','\t', 'FileType','text');
+    eeg_chans = locs.Var4;
+else
+    eeg_chans = '';
+end
 
-    function isEEG = is_eeg_chan(chan)
-        template_file = fullfile(fileparts(which('eeglab')),'sample_locs/Standard-10-20-Cap81.locs');
-        if exist(template_file, 'file')
-            locs = readtable(template_file,'Delimiter','\t', 'FileType','text');
-            eeg_chans = locs.Var4;
-            isEEG = any(strcmpi(chan, eeg_chans));
-        else
-            warning('Could not find standard 10-20 channel locations file. Failed to determine if channel is EEG using names.')
-            isEEG = false;
-        end
+% Assign channel types based on channel name
+% types retrived from https://bids-specification.readthedocs.io/en/stable/glossary.html#objects.columns.type__eeg_channels
+types = {'EEG', 'EMG', 'EOG', 'ECG', 'EKG', 'TRIG', 'GSR', 'PPG', 'MISC'};
+for i = 1:length(EEG.chanlocs)
+    label = EEG.chanlocs(i).labels;
+    matchIdx = cellfun(@(x) contains(lower(label), lower(x)), types);
+
+    if any(matchIdx)
+        EEG.chanlocs(i).type = types{matchIdx};
+    elseif any(strcmpi(label, eeg_chans))
+        EEG.chanlocs(i).type = 'EEG';
     end
 end
+
