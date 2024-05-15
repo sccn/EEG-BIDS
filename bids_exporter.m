33a2b68825b0e8556a9b30e19b3b6df1311303ae
Dung Truong <dt.young112@gmail.com>
2024-05-15 10:05:42 -0400

    allow executing script for bids export wizard

--- bids_exporter.m
+++ bids_exporter.m
@@ -1,10 +1,14 @@
-function bids_exporter()
-[res userdata err structout] = inputgui('geometry', { 1 }, 'uilist', ...
-                          { { 'style' 'text' 'string' 'BIDS exporter tool' }});
-
-if isempty(err), return; end
-[STUDY, ALLEEG] = pop_studywizard();
-
-if ~isempty(ALLEEG)
-    pop_exportbids(STUDY, ALLEEG)
-end
+function bids_exporter(varargin)
+    if nargin == 0
+        [res userdata err structout] = inputgui('geometry', { 1 }, 'uilist', ...
+                                  { { 'style' 'text' 'string' 'BIDS exporter tool' }});
+        
+        if isempty(err), return; end
+        [STUDY, ALLEEG] = pop_studywizard();
+        
+        if ~isempty(ALLEEG)
+            pop_exportbids(STUDY, ALLEEG)
+        end
+    elseif nargin == 1 && exist(varargin{1}, 'file')
+        pop_runscript(varargin{1});
+    end
